e7f33bd7b853ad2671f2b1d2138ee16d02d950dc
FroggyKwa <ilya.laguxa@gmail.com>
2024-03-22 01:30:57 +1000

    add line text representation

--- Database/Entities/Line.cs
+++ Database/Entities/Line.cs
@@ -6,13 +6,13 @@ namespace FlagLearner.Database.Entities
     public partial class Line
     {
         public long Id { get; set; }
-        public string Line1 { get; set; } = null!;
+        public string Name { get; set; } = null!;
         public long? CountryId { get; set; }
 
         public virtual Country? Country { get; set; }
         public override string ToString()
         {
-            return Line1;
+            return Name;
         }
     }
 }
