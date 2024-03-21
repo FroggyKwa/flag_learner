80bb7eb3fbae4e40336e947313e4f2b9c2dd55ae
FroggyKwa <ilya.laguxa@gmail.com>
2024-03-22 01:33:58 +1000

    create viewmodel, create flag list

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FlagLearner.Database;
+using FlagLearner.Database.Entities;
+using FlagLearner.Database.Repository;
+using FlagLearner.Views.Common;
+
+namespace FlagLearner.ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly CountriesContext _db = null!;
+        private readonly CountryRepository _countryRepository = null!;
+        public List<Country> countries { get; } = null!;
+        public int selectedCountryId { get; set; }
+        public Country? selectedCountry { 
+            get => _countryRepository.GetItem(selectedCountryId); }
+
+        public MainViewModel()
+        {
+            _db = new CountriesContext();
+            _countryRepository = new CountryRepository(_db); 
+            countries = _countryRepository.queryBuilder.GetAll().Build();
+        }
+    }
+}
