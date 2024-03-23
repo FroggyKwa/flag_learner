09ab508ca3d91d98ced93f12fbe1c69c14cd1100
FroggyKwa <ilya.laguxa@gmail.com>
2024-03-23 16:36:40 +1000

    add toModel and toDomain converters

---
+++ Database/Converters/CountryConverter.cs
@@ -0,0 +1,65 @@
+﻿using FlagLearner.Database.Entities;
+
+namespace FlagLearner.Database.Converters
+{
+
+    public static class CountryConverter
+    {
+        public class DomainCountryModel
+        {
+            public long Id;
+            public long? CountryInfoId;
+            public string CountryName;
+            public string ImageUrl;
+            public CountryInfo CountryInfo;
+            public List<string> Colors;
+            public List<string> Lines;
+
+            public DomainCountryModel(
+                long id,
+                long? countryInfoId,
+                string countryName,
+                string imageUrl,
+                CountryInfo countryInfo,
+                List<string> colors,
+                List<string> lines)
+            {
+                Id = id;
+                CountryInfoId = countryInfoId;
+                CountryName = countryName;
+                ImageUrl = imageUrl;
+                CountryInfo = countryInfo;
+                Colors = colors;
+                Lines = lines;
+            }
+
+            public override string ToString()
+            {
+                return CountryName;
+            }
+
+        }
+        public static DomainCountryModel ToDomain(this Country country)
+        {
+            return new DomainCountryModel(id: country.Id,
+                countryInfoId: country.CountryInfoId,
+                countryName: country.CountryName,
+                imageUrl: country.ImageUrl,
+                countryInfo: country.CountryInfo,
+                colors: country.Colors.Select(item => item.ColorName).ToList(),
+                lines: country.Lines.Select(item => item.Name).ToList());
+        }
+
+        public static Country ToModel(this DomainCountryModel country)
+        {
+            return new Country
+            {
+                Id = country.Id,
+                CountryInfoId = country.CountryInfoId,
+                CountryName = country.CountryName,
+                ImageUrl = country.ImageUrl,
+                CountryInfo = country.CountryInfo,
+            };
+        }
+    }
+}
---
+++ Database/Converters/LineConverter.cs
@@ -0,0 +1,14 @@
+﻿using FlagLearner.Database.Entities;
+using System.Runtime.CompilerServices;
+
+namespace FlagLearner.Database.Converters
+{
+    
+    public static class LineConverter
+    {
+        public static string ToDomain(this Line line)
+        {
+            return line.ToString();
+        }
+    }
+}
